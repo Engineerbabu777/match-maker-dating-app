0437f859530fb15c40e9d3f0d69bbf2b63cf8fe3
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-11 23:50:42 +0500

    Added-login-backend

--- api/index.js
+++ api/index.js
@@ -93,3 +93,62 @@ app.post("/register", async (req, res) => {
       console.log("Error sending the verification email");
     }
   };
+
+
+
+  
+//verify the user
+app.get("/verify/:token", async (req, res) => {
+    try {
+      const token = req.params.token;
+  
+      const user = await User.findOne({ verificationToken: token });
+      if (!user) {
+        return res.status(404).json({ message: "Invalid verification token" });
+      }
+  
+      //mark the user as verified
+      user.verified = true;
+      user.verificationToken = undefined;
+  
+      await user.save();
+  
+      res.status(200).json({ message: "Email verified Sucesfully" });
+    } catch (error) {
+      console.log("errror", error);
+      res.status(500).json({ message: "Email verification failed" });
+    }
+  });
+  
+  const generateSecretKey = () => {
+    const secretKey = crypto.randomBytes(32).toString("hex");
+  
+    return secretKey;
+  };
+  
+  const secretKey = generateSecretKey();
+  
+  //endpoint to login
+  app.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      //check if the user exists already
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+  
+      //check in password is correct
+      if (user.password !== password) {
+        return res.status(401).json({ message: "Invalide password" });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, secretKey);
+  
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ message: "login failed" });
+    }
+  });
+  
