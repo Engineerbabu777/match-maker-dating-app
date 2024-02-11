08e0541d43ba2ede65f05507aa8243d6d82ba399
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-02-11 23:46:03 +0500

    Added-register-ui

--- api/index.js
+++ api/index.js
@@ -32,3 +32,64 @@ mongoose
 app.listen(port, () => {
   console.log("Server is running on 3000");
 });
+
+
+//endpoint to register a user to the backend
+app.post("/register", async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+  
+      //check if the email is already registered
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        console.log("Email already registered");
+        return res.status(400).json({ message: "Email already registered" });
+      }
+  
+      //create a new User
+      const newUser = new User({
+        name,
+        email,
+        password,
+      });
+  
+      //generate the verification token
+      newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+  
+      //save the user to the database
+      await newUser.save();
+  
+      //send the verification email to the registered user
+      sendVerificationEmail(newUser.email, newUser.verificationToken);
+  
+      res
+        .status(200)
+        .json({ message: "User registered successfully", userId: newUser._id });
+    } catch (error) {
+      console.log("Error registering user", error);
+      res.status(500).json({ message: "Registration failed" });
+    }
+  });
+  const sendVerificationEmail = async (email, verificationToken) => {
+    const transpoter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
+  
+    const mailOptions = {
+      from: process.env.FROM,
+      to: email,
+      subject: "Email verification",
+      text: `Please click on the following link to verify your email : http://localhost:3000/verify/${verificationToken}`,
+    };
+  
+    //send the mail
+    try {
+      await transpoter.sendMail(mailOptions);
+    } catch (error) {
+      console.log("Error sending the verification email");
+    }
+  };
